5a1f8e8dbef5b80cf96a91d37601a5a384971479
kevin <kevindbaik@gmail.com>
2024-01-14 23:47:20 -0800

    created serialize and deserialize

--- src/main/java/learn/solar/data/PanelFileRepository.java
+++ src/main/java/learn/solar/data/PanelFileRepository.java
@@ -15,6 +15,33 @@ public class PanelFileRepository {
         this.filePath = filePath;
     }
 
+    public String serialize(Panel panel) {
+        return String.join(",",
+                String.valueOf(panel.getId()),
+                panel.getSection(),
+                String.valueOf(panel.getRow()),
+                String.valueOf(panel.getColumn()),
+                String.valueOf(panel.getInstallationYear()),
+                panel.getMaterial().getAbbreviation(),
+                String.valueOf(panel.isTracking())
+        );
+    }
+
+    public Panel deserialize(String line) {
+        String[] fields = line.split(",");
+        if(fields.length == 7) {
+            Panel panel = new Panel();
+            panel.setId(Integer.parseInt(fields[0]));
+            panel.setSection(fields[1]);
+            panel.setRow(Integer.parseInt(fields[2]));
+            panel.setColumn(Integer.parseInt(fields[3]));
+            panel.setInstallationYear(Integer.parseInt(fields[4]));
+            panel.setMaterial(Material.findByAbbreviation(fields[5]));
+            panel.setTracking(Boolean.parseBoolean(fields[6]));
+            return panel;
+        }
+        return null;
+    }
     public List<Panel> findAll() {
         ArrayList<Panel> result = new ArrayList<>();
         try(BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
